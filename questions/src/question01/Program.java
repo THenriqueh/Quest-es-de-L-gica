cb8a80d563b3b5ccc1b602cce761a4fa13904851
Thallis Ferreira <thallis.ferreira2019@gmail.com>
2022-07-14 05:59:32 -0300

    Resolução do Problema

---
+++ questions/src/question01/Program.java
@@ -0,0 +1,21 @@
+package question01;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		int indice = 13;
+		int sum = 0;
+		int k = 0;
+
+		while( k < indice) { 
+		k = k + 1;
+		sum = sum + k;
+
+		}
+
+		System.out.println(sum);
+
+	}
+
+}
---
+++ questions/src/question05/Program.java
@@ -0,0 +1,28 @@
+package question05;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Escreva algo que você queira inverter: ");
+		String str = sc.nextLine();
+		System.out.println(str);
+		char[] ch = str.toCharArray();
+		int begin = 0;
+		int end = ch.length - 1;
+		char temp;
+		while (end > begin) {
+			temp = ch[begin];
+			ch[begin] = ch[end];
+			ch[end] = temp;
+			end--;
+			begin++;
+		}
+		str = new String(ch);
+		System.out.println(str);
+		sc.close();
+	}
+
+}
